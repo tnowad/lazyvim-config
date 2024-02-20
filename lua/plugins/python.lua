027973bc4c9f2797eebc5bbe429295fce39c478f
Nguyen Minh Tuan <tnowad@gmail.com>
2024-02-20 22:06:52 +0700

    feat: add neotest for python

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -35,6 +35,22 @@ return {
     },
   },
   {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "nvim-neotest/neotest-python",
+    },
+    opts = {
+      adapters = {
+        ["neotest-python"] = {
+          -- Here you can specify the settings for the adapter, i.e.
+          runner = "pytest",
+          python = ".venv/bin/python",
+        },
+      },
+    },
+  },
+  {
     "mfussenegger/nvim-dap",
     optional = true,
     dependencies = {
