7a7098c54899064744cca9bd500048ce0d9f2340
Nguyen Minh Tuan <tnowad@gmail.com>
2024-02-21 22:21:23 +0700

    feat: add lint cmake

--- lua/plugins/cmake.lua
+++ lua/plugins/cmake.lua
@@ -17,4 +17,20 @@ return {
       })
     end,
   },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        cmake = { "cmakelint" },
+      },
+    },
+  },
+  {
+    "mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "cmakelang", "cmakelint" })
+    end,
+  },
 }
