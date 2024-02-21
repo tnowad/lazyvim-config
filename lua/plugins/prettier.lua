23f3a56fa0c0b75f759c6b3f627e84246b54d722
Nguyen Minh Tuan <tnowad@gmail.com>
2024-02-21 22:20:37 +0700

    feat: add format file with prettier

--- lua/plugins/prettier.lua
+++ lua/plugins/prettier.lua
@@ -14,4 +14,28 @@ return {
       table.insert(opts.sources, nls.builtins.formatting.prettier)
     end,
   },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["javascript"] = { "prettier" },
+        ["javascriptreact"] = { "prettier" },
+        ["typescript"] = { "prettier" },
+        ["typescriptreact"] = { "prettier" },
+        ["vue"] = { "prettier" },
+        ["css"] = { "prettier" },
+        ["scss"] = { "prettier" },
+        ["less"] = { "prettier" },
+        ["html"] = { "prettier" },
+        ["json"] = { "prettier" },
+        ["jsonc"] = { "prettier" },
+        ["yaml"] = { "prettier" },
+        ["markdown"] = { "prettier" },
+        ["markdown.mdx"] = { "prettier" },
+        ["graphql"] = { "prettier" },
+        ["handlebars"] = { "prettier" },
+      },
+    },
+  },
 }
