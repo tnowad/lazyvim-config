ec2ecd7e74a99a13d67022eef02dafe3b36c97ba
Nguyen Minh Tuan <tnowad@gmail.com>
2024-02-23 22:25:30 +0700

    feat: add markdown preview

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -42,4 +42,24 @@ return {
     },
   },
 
+  -- Markdown preview
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    keys = {
+      {
+        "<leader>cp",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
+      },
+    },
+    config = function()
+      vim.cmd([[do FileType]])
+    end,
+  },
+
 }
