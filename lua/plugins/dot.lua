f0b961c71bdfbb8e62adeed779389e949abbbfb8
Nguyen Minh Tuan <tnowad@gmail.com>
2024-02-25 22:36:56 +0700

    chore: remove unused pattern

--- lua/plugins/dot.lua
+++ lua/plugins/dot.lua
@@ -46,8 +46,6 @@ return {
       vim.filetype.add({
         extension = { rasi = "rasi" },
         pattern = {
-          [".*/waybar/config"] = "jsonc",
-          [".*/mako/config"] = "dosini",
           [".*/kitty/*.conf"] = "bash",
         },
       })
