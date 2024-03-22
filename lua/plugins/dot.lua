477945706dcc9fcde1d79229c5de52bf274694f8
Nguyen Minh Tuan <tnowad@gmail.com>
2024-03-22 07:57:45 +0700

    chore: remove unused config

--- lua/plugins/dot.lua
+++ lua/plugins/dot.lua
@@ -7,33 +7,6 @@ local function have(path)
 end
 
 return {
-
-  -- Add Hyprland Parser
-  {
-    "luckasRanarison/tree-sitter-hyprlang",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      opts = function(_, opts)
-        require("nvim-treesitter.parsers").get_parser_configs().hyprlang = {
-          install_info = {
-            url = "https://github.com/luckasRanarison/tree-sitter-hyprlang",
-            files = { "src/parser.c" },
-            branch = "master",
-          },
-          filetype = "hyprlang",
-        }
-
-        opts.ensure_installed = opts.ensure_installed or {}
-        vim.list_extend(opts.ensure_installed, { "hyprlang" })
-      end,
-    },
-    enabled = function()
-      return have("hypr")
-    end,
-    event = "BufRead */hypr/*.conf",
-  },
-
-  -- add some stuff to treesitter
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
