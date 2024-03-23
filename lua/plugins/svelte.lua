387283555fd5f2bc2a7b1b0af5259a172e92f1a9
Nguyen Minh Tuan <tnowad@gmail.com>
2024-03-24 05:27:15 +0700

    feat: add svelte plugin

---
+++ lua/plugins/svelte.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "svelte" })
+      end
+    end,
+  },
+
+  {
+    "mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "svelte-language-server" })
+    end,
+  },
+}
